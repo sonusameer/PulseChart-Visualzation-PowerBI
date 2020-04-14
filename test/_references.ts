5702a245c35954ae367c496ba136cebd58871b91
SK.SAMEERUDDIN <shaik.18bcd7140@vitap.ac.in>
2020-04-14 16:42:03 +0530

    Create _references.ts

---
+++ test/_references.ts
@@ -0,0 +1,22 @@
+// Power BI API
+/// <reference path="../.api/v2.1.0/PowerBI-visuals.d.ts" />
+
+// Power BI Extensibility
+/// <reference path="../node_modules/powerbi-visuals-utils-dataviewutils/lib/index.d.ts" />
+/// <reference path="../node_modules/powerbi-visuals-utils-colorutils/lib/index.d.ts" />
+/// <reference path="../node_modules/powerbi-visuals-utils-typeutils/lib/index.d.ts" />
+/// <reference path="../node_modules/powerbi-visuals-utils-svgutils/lib/index.d.ts" />
+/// <reference path="../node_modules/powerbi-visuals-utils-interactivityutils/lib/index.d.ts" />
+/// <reference path="../node_modules/powerbi-visuals-utils-tooltiputils/lib/index.d.ts" />
+/// <reference path="../node_modules/powerbi-visuals-utils-dataviewutils/lib/index.d.ts" />
+/// <reference path="../node_modules/powerbi-visuals-utils-formattingutils/lib/index.d.ts" />
+/// <reference path="../node_modules/powerbi-visuals-utils-chartutils/lib/index.d.ts"/>
+/// <reference path="../node_modules/powerbi-visuals-utils-testutils/lib/index.d.ts"/>
+
+// The visual
+/// <reference path="../.tmp/drop/visual.d.ts" />
+
+// Test
+/// <reference path="helpers.ts" />
+/// <reference path="visualData.ts" />
+/// <reference path="visualBuilder.ts" />
